a4c07571419c780e24d91cda7fa91e091c7900eb
ecesarini <73652902+ecesarini@users.noreply.github.com>
2022-08-01 00:22:35 +0200

    fixed css about home page
    JS navbar clicked. Navigation JS.

--- static/js/stmpw.js
+++ static/js/stmpw.js
@@ -1,11 +1,21 @@
 // TODO:
 //  Very hard project, start from building class to create HTML autonomous containers @stmphtmlconstructor.js;
 
-import { StmpwJsonImporter } from "./stmpwjsonimporter.js";
-import { StmpHTMLConstructor } from "./stmpwhtmlconstructor.js";
+//import { StmpwJsonImporter } from "./stmpwjsonimporter.js";
+//import { StmpHTMLConstructor } from "./stmpwhtmlconstructor.js";
+let navClickedOn = false;
+
+function navClickedAction() {
+    navClickedOn = true;
+
+}
 
 (function stmpw() {
-    const stmpwjsonimporter = new StmpwJsonImporter('./data.json'),
-        jsonData = stmpwjsonimporter.getJsonData();
-    new StmpHTMLConstructor(jsonData);
+    const navClicked = document.querySelectorAll('.navbar-conten-elemet');
+
+    navClicked.forEach(n => n.addEventListener('click', navClickedAction)); 
+
+    //const stmpwjsonimporter = new StmpwJsonImporter('./data.json'),
+    //    jsonData = stmpwjsonimporter.getJsonData();
+    //new StmpHTMLConstructor(jsonData);
 }) ();
---
+++ static/js/stmpw.orig
@@ -0,0 +1,11 @@
+// TODO:
+//  Very hard project, start from building class to create HTML autonomous containers @stmphtmlconstructor.js;
+
+import { StmpwJsonImporter } from "./stmpwjsonimporter.js";
+import { StmpHTMLConstructor } from "./stmpwhtmlconstructor.js";
+
+(function stmpw() {
+    const stmpwjsonimporter = new StmpwJsonImporter('./data.json'),
+        jsonData = stmpwjsonimporter.getJsonData();
+    new StmpHTMLConstructor(jsonData);
+}) ();
